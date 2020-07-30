fde7e2479fc39638e48867f57c90f2dea0a2c7a8
SaqlainAbid-star <jamalisaqlain7@gmail.com>
2020-07-30 12:54:30 +0500

    https://github.com/SaqlainAbid-star/C-Programming-Quiz.git

---
+++ app.js
@@ -0,0 +1,160 @@
+function check(){
+    var score=0;
+    var right_answer = document.getElementById("q1-a4");
+    // var q1_answer_2 = document.getElementById("q1-a2");
+    // var q1_answer_3 = document.getElementById("q1-a3");
+    // var q1_answer_4 = document.getElementById("q1-a4");
+    if(right_answer.checked == true){
+        score++;
+        alert("Q1 answer is right")
+    }
+    else{
+        alert("Q1 answer is wrong")
+    }
+
+
+
+    var right_answer = document.getElementById("q2-a1");
+    // var q2_answer_2 = document.getElementById("q2-a2");
+    // var q2_answer_3 = document.getElementById("q2-a3");
+    // var q2_answer_4 = document.getElementById("q3-a4");
+    if(right_answer.checked == true){
+        score++;
+        alert("Q2 answer is right")
+    }
+    else{
+        alert("Q2 answer is wrong")
+    }
+
+   
+
+
+
+    var right_answer = document.getElementById("q3-a1");
+    // var q2_answer_2 = document.getElementById("q2-a2");
+    // var q2_answer_3 = document.getElementById("q2-a3");
+    // var q2_answer_4 = document.getElementById("q3-a4");
+    if(right_answer.checked == true){
+        score++;
+        alert("Q3 answer is right")
+    }
+    else{
+        alert("Q3 answer is wrong")
+    }
+
+ 
+
+
+
+    var right_answer = document.getElementById("q4-a4");
+    // var q2_answer_2 = document.getElementById("q2-a2");
+    // var q2_answer_3 = document.getElementById("q2-a3");
+    // var q2_answer_4 = document.getElementById("q3-a4");
+    if(right_answer.checked == true){
+        score++;
+        alert("Q4 answer is right")
+    }
+    else{
+        alert("Q4 answer is wrong")
+    }
+
+
+
+
+
+    var right_answer = document.getElementById("q5-a3");
+    // var q2_answer_2 = document.getElementById("q2-a2");
+    // var q2_answer_3 = document.getElementById("q2-a3");
+    // var q2_answer_4 = document.getElementById("q3-a4");
+    if(right_answer.checked == true){
+        score++;
+        alert("Q5 answer is right")
+    }
+    else{
+        alert("Q5 answer is wrong")
+    }
+
+    
+
+
+
+    var right_answer = document.getElementById("q6-a2");
+    // var q2_answer_2 = document.getElementById("q2-a2");
+    // var q2_answer_3 = document.getElementById("q2-a3");
+    // var q2_answer_4 = document.getElementById("q3-a4");
+    if(right_answer.checked == true){
+        score++;
+        alert("Q6 answer is right")
+    }
+    else{
+        alert("Q6 answer is wrong")
+    }
+
+   
+
+
+
+
+    var right_answer = document.getElementById("q7-a2");
+    // var q2_answer_2 = document.getElementById("q2-a2");
+    // var q2_answer_3 = document.getElementById("q2-a3");
+    // var q2_answer_4 = document.getElementById("q3-a4");
+    if(right_answer.checked == true){
+        score++;
+        alert("Q7 answer is right")
+    }
+    else{
+        alert("Q7 answer is wrong")
+    }
+
+   
+
+
+
+
+
+    var right_answer = document.getElementById("q8-a3");
+    // var q2_answer_2 = document.getElementById("q2-a2");
+    // var q2_answer_3 = document.getElementById("q2-a3");
+    // var q2_answer_4 = document.getElementById("q3-a4");
+    if(right_answer.checked == true){
+        score++;
+        alert("Q8 answer is right")
+    }
+    else{
+        alert("Q8 answer is wrong")
+    }
+
+
+
+
+    var right_answer = document.getElementById("q9-a2");
+    // var q2_answer_2 = document.getElementById("q2-a2");
+    // var q2_answer_3 = document.getElementById("q2-a3");
+    // var q2_answer_4 = document.getElementById("q3-a4");
+    if(right_answer.checked == true){
+        score++;
+        alert("Q9 answer is right")
+    }
+    else{
+        alert("Q9 answer is wrong")
+    }
+
+
+    var right_answer = document.getElementById("q10-a4");
+    // var q2_answer_2 = document.getElementById("q2-a2");
+    // var q2_answer_3 = document.getElementById("q2-a3");
+    // var q2_answer_4 = document.getElementById("q3-a4");
+    if(right_answer.checked == true){
+        score++;
+        alert("Q10 answer is right")
+    }
+    else{
+        alert("Q10 answer is wrong")
+    }
+
+    alert("Your Total Score is "+ score++ + " Out Of 10");
+   
+
+
+}
